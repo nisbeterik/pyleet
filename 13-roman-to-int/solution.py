b32e4db757ab137897e9af618128cf8694e89c0f
Erik Nisbet <eriknisbet8@gmail.com>
2026-03-02 21:28:08 +0100

    add solution for romantoint

--- 13-roman-to-int/solution.py
+++ 13-roman-to-int/solution.py
@@ -1,6 +1,6 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        rom_dick = {
+        translations = {
             "I": 1,
             "V": 5,
             "X": 10,
@@ -9,11 +9,13 @@ class Solution:
             "D": 500,
             "M": 1000
         }
-        
-        total = 0
-        for i in range(len(s)):
-            total+=rom_dick[s[i]]
-        return total
+        number = 0
+        s = s.replace("IV", "IIII").replace("IX", "VIIII")
+        s = s.replace("XL", "XXXX").replace("XC", "LXXXX")
+        s = s.replace("CD", "CCCC").replace("CM", "DCCCC")
+        for char in s:
+            number += translations[char]
+        return number
         
 
 def main():
